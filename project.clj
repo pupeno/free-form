427e1d559d1d0b28039c84c33f645c13084187b8
J. Pablo Fernández <pupeno@carouselapps.com>
2015-10-15 10:46:18 +0100

    Next version will be 0.1.1.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 ;;;; Copyright © 2015 Carousel Apps, Ltd. All rights reserved.
 
-(defproject com.carouselapps/free-form "0.1.0"
+(defproject com.carouselapps/free-form "0.1.1-SNAPSHOT"
   :description "Library for building forms with Reagent or Re-frame."
   :url "https://carouselapps.com/free-form"
   :license {:name "Eclipse Public License"
