375ac31c7e2e969e889689d3cc393070d5c752bd
J. Pablo Fernández <pupeno@pupeno.com>
2016-08-22 18:45:43 +0100

    Point to the new namespace for the library.

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
 ;;;; Copyright © 2015 Carousel Apps, Ltd. All rights reserved.
 
 (defproject com.carouselapps/free-form "0.2.1-SNAPSHOT"
-  :description "Library for building forms with Reagent or Re-frame."
-  :url "https://carouselapps.com/free-form"
+  :description "Library for building forms with Reagent or Re-frame. This library changed namespace to com.pupeno/free-form"
+  :url "https://github.com/pupeno/free-form"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :lein-release {:deploy-via :clojars}
   :signing {:gpg-key "F2FB1C6F"}
   :scm {:name "git"
-        :url  "https://github.com/carouselapps/free-form"}
+        :url  "https://github.com/pupeno/free-form"}
 
   :dependencies [[org.clojure/clojurescript "1.7.48"]
                  [com.carouselapps/re-frame "0.4.1"]
