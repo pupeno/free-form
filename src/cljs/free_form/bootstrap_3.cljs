d7c46fe9dcc85b5b351c9d2d86d16dd251268ef7
J. Pablo Fernández <pupeno@pupeno.com>
2017-02-05 15:30:58 +0000

    Add support for checkbox and radio buttons in the Bootstrap 3 extension.

--- src/cljs/free_form/bootstrap_3.cljs
+++ src/cljs/free_form/bootstrap_3.cljs
@@ -21,6 +21,16 @@
     :textarea [:textarea.form-control {:free-form/input {:keys keys}
                                        :type            type
                                        :id              id}]
+    :checkbox [:input {:free-form/input {:keys keys}
+                       :type            type
+                       :id              id}]
+    :radio (map (fn [[value name]]
+                  ^{:name name}
+                  [:input {:free-form/input {:keys keys}
+                           :type            type
+                           :name            id
+                           :value           value}])
+                (partition 2 options))
     [:input.form-control {:free-form/input {:keys keys}
                           :type            type
                           :id              id
@@ -30,32 +40,88 @@
   (if (field? node)
     (let [{:keys [type keys extra-validation-error-keys label placeholder options]} (key->keys (second node))
           id (s/join "-" (map name keys))]
-      [:div.form-group {:free-form/error-class {:keys keys :extra-keys extra-validation-error-keys :error "has-error"}}
-       [:label.control-label {:for id} label]
-       (expand-bootstrap-3-input id keys type placeholder options)
-       [:div.text-danger {:free-form/error-message {:keys keys}} [:p]]])
+      (case type
+        :checkbox [:div.checkbox {:free-form/error-class {:keys       keys
+                                                          :extra-keys extra-validation-error-keys
+                                                          :error      "has-error"}}
+                   [:label (expand-bootstrap-3-input id keys type placeholder options) label]
+                   [:div.text-danger {:free-form/error-message {:keys keys}} [:p]]]
+        :radio [:div {:free-form/error-class {:keys       keys
+                                              :extra-keys extra-validation-error-keys
+                                              :error      "has-error"}}
+                [:label label]
+                (map (fn [input]
+                       ^{:key (str id "-" (get-in input [1 :value]))}
+                       [:div.radio
+                        [:label input (:name (meta input))]])
+                     (expand-bootstrap-3-input id keys type placeholder options))
+                [:div.text-danger {:free-form/error-message {:keys keys}} [:p]]]
+        [:div.form-group {:free-form/error-class {:keys keys :extra-keys extra-validation-error-keys :error "has-error"}}
+         [:label.control-label {:for id} label]
+         (expand-bootstrap-3-input id keys type placeholder options)
+         [:div.text-danger {:free-form/error-message {:keys keys}} [:p]]]))
     node))
 
 (defn- expand-bootstrap-3-horizontal-fields [node]
   (if (field? node)
     (let [{:keys [type keys extra-validation-error-keys label placeholder options]} (key->keys (second node))
           id (s/join "-" (map name keys))]
-      [:div.form-group {:free-form/error-class {:keys keys :extra-keys extra-validation-error-keys :error "has-error"}}
-       [:label.col-sm-2.control-label {:for id} label]
-       [:div.col-sm-10 (expand-bootstrap-3-input id keys type placeholder options)
-        [:div.text-danger {:free-form/error-message {:keys keys}} [:p]]]])
+      (case type
+        :checkbox [:div.form-group {:free-form/error-class {:keys       keys
+                                                            :extra-keys extra-validation-error-keys
+                                                            :error      "has-error"}}
+                   [:div.col-sm-offset-2.col-sm-10
+                    [:div.checkbox
+                     [:label (expand-bootstrap-3-input id keys type placeholder options) label]]
+                    [:div.text-danger {:free-form/error-message {:keys keys}} [:p]]]]
+        :radio [:div.form-group {:free-form/error-class {:keys       keys
+                                                         :extra-keys extra-validation-error-keys
+                                                         :error      "has-error"}}
+                [:label.col-sm-2.control-label label]
+                (-> [:div.col-sm-10]
+                    (into (map (fn [input]
+                                 ^{:key (str id "-" (get-in input [1 :value]))}
+                                 [:div.radio
+                                  [:label input (:name (meta input))]])
+                               (expand-bootstrap-3-input id keys type placeholder options)))
+                    (conj [:div.text-danger {:free-form/error-message {:keys keys}} [:p]]))]
+        [:div.form-group {:free-form/error-class {:keys       keys
+                                                  :extra-keys extra-validation-error-keys
+                                                  :error      "has-error"}}
+         [:label.col-sm-2.control-label {:for id} label]
+         [:div.col-sm-10 (expand-bootstrap-3-input id keys type placeholder options)
+          [:div.text-danger {:free-form/error-message {:keys keys}} [:p]]]]))
     node))
 
 (defn- expand-bootstrap-3-inline-fields [node]
   (if (field? node)
     (let [{:keys [type keys extra-validation-error-keys label placeholder options]} (key->keys (second node))
           id (s/join "-" (map name keys))]
-      [:div.form-group {:free-form/error-class {:keys keys :extra-keys extra-validation-error-keys :error "has-error"}}
-       [:label.control-label {:for id} label]
-       " "
-       (expand-bootstrap-3-input id keys type placeholder options)
-       " "
-       [:div.text-danger {:free-form/error-message {:keys keys}} [:p]]])
+      (case type
+        :checkbox [:div.form-group
+                   [:label.checkbox-inline {:free-form/error-class {:keys       keys
+                                                                    :extra-keys extra-validation-error-keys
+                                                                    :error      "has-error"}}
+                    (expand-bootstrap-3-input id keys type placeholder options) label]
+                   [:div.text-danger {:free-form/error-message {:keys keys}} [:p]]]
+        :radio [:div.form-group {:free-form/error-class {:keys       keys
+                                                         :extra-keys extra-validation-error-keys
+                                                         :error      "has-error"}}
+                (when label [:label label])
+                (map (fn [input]
+                       ^{:key (str id "-" (get-in input [1 :value]))}
+                       [:div.radio-inline
+                        [:label input (:name (meta input))]])
+                     (expand-bootstrap-3-input id keys type placeholder options))
+                [:div.text-danger {:free-form/error-message {:keys keys}} [:p]]]
+        [:div.form-group {:free-form/error-class {:keys       keys
+                                                  :extra-keys extra-validation-error-keys
+                                                  :error      "has-error"}}
+         [:label.control-label {:for id} label]
+         " "
+         (expand-bootstrap-3-input id keys type placeholder options)
+         " "
+         [:div.text-danger {:free-form/error-message {:keys keys}} [:p]]]))
     node))
 
 (defn- bootstrap-3-form-horizontal? [node]
